e1adde5d45d203c3aae803680d7feec49fe11f7e
Caffeinated Programmer <caffeinatedprogrammer@outlook.com>
2020-07-19 16:54:44 +0800

    Fix eslint error

--- src/tutorials/index.js
+++ src/tutorials/index.js
@@ -1,21 +1,21 @@
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 
 export default {
-    list: {
-        "intro": {
-            title: "Introduction",
-            description: "Introduction to Caffeinated Tutorials",
-            content: readFileSync(__dirname + "/intro.md", "utf-8")
-        },
-        "limit": {
-            title: "Limit",
-            description: "Introduction to Limit",
-            content: readFileSync(__dirname + "/limit.md", "utf-8")
-        },
-        "limit_thm": {
-            title: "Computation rules of limits",
-            description: "Computation rules and proofs",
-            content: readFileSync(__dirname + "/limit_thm.md", "utf-8")
-        }
-    }
-}
+  list: {
+    intro: {
+      title: 'Introduction',
+      description: 'Introduction to Caffeinated Tutorials',
+      content: readFileSync(`${__dirname}/intro.md`, 'utf-8'),
+    },
+    limit: {
+      title: 'Limit',
+      description: 'Introduction to Limit',
+      content: readFileSync(`${__dirname}/limit.md`, 'utf-8'),
+    },
+    limit_thm: {
+      title: 'Computation rules of limits',
+      description: 'Computation rules and proofs',
+      content: readFileSync(`${__dirname}/limit_thm.md`, 'utf-8'),
+    },
+  },
+};
